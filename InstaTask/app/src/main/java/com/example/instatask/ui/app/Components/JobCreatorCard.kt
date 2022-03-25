388a066edf30be86c579212ced72de8b2d3514de
Carlos Castellanos <96276293+carloscastellanos271@users.noreply.github.com>
2022-03-25 16:54:24 -0700

    Create JobCreatorCard.kt

---
+++ InstaTask/app/src/main/java/com/example/instatask/ui/app/Components/JobCreatorCard.kt
@@ -0,0 +1,68 @@
+package com.example.instatask.ui.app.Components
+
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+fun cardViewCallBack(context: Context, name: String) {
+    Toast.makeText(context,"Hello ${name}", Toast.LENGTH_LONG).show()
+}
+
+@Composable
+fun JobCreatorCard(name:String,description:String,image:Int)
+{
+    var context= LocalContext.current
+    Card(
+        modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth()
+            .wrapContentHeight().
+            clickable { cardViewCallBack(context,name+description) },
+        shape = MaterialTheme.shapes.medium,
+        elevation = 5.dp,
+        backgroundColor = MaterialTheme.colors.surface
+    ) {
+
+        Row (
+            verticalAlignment = Alignment.CenterVertically
+
+        ){
+
+            Image(painter = painterResource(id = image),
+                contentDescription =null,
+                modifier= Modifier
+                    .size(130.dp)
+                    .padding(8.dp),
+                contentScale = ContentScale.Fit
+            )
+            Column(Modifier.padding(8.dp)) {
+
+                Text(text = name,
+                    style= MaterialTheme.typography.h6,
+                    color= MaterialTheme.colors.onSurface,
+                    modifier= Modifier.padding(start=30.dp)
+                )
+                Text(text = description,
+                    style= MaterialTheme.typography.body1,
+                    color= MaterialTheme.colors.onSurface,
+                    modifier= Modifier.padding(start=15.dp)
+                )
+
+            }
+
+        }
+
+    }
+}
