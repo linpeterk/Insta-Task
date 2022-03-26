cf83c726c6f3019aca572cce8f0166e501216b1c
Carlos Castellanos <96276293+carloscastellanos271@users.noreply.github.com>
2022-03-25 20:13:37 -0700

    Update WhenJob.kt

--- InstaTask/app/src/main/java/com/example/instatask/ui/app/screens/WhenJob.kt
+++ InstaTask/app/src/main/java/com/example/instatask/ui/app/screens/WhenJob.kt
@@ -27,10 +27,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.instatask.ui.app.Components.AllReviews
-import com.example.instatask.ui.app.Components.MakeGoogleMap
-import com.example.instatask.ui.app.Components.TopBar
-import com.example.instatask.ui.app.Components.jobCreators
+import com.example.instatask.R
+import com.example.instatask.ui.app.Components.*
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
@@ -53,7 +51,7 @@ private const val TAG = "MapSampleActivity"
 @Composable
 fun WhenJobClicked(navController: NavController) {
 
-
+    var context= LocalContext.current
     // cardCount =  remember {  mutableStateOf(dataBase.count()) }
     val composableScope = rememberCoroutineScope()
 //        Log.d(Examples.TAG,"Hello from Main")
@@ -107,6 +105,56 @@ fun WhenJobClicked(navController: NavController) {
                                 Text(text = "Report", modifier = Modifier.padding(5.dp), color = Color.White)
                             }
                         }
+                        Box(modifier = Modifier
+                            .weight(0.8f)
+                            .background(MaterialTheme.colors.primaryVariant)
+                        ){
+//                                MakeScrollComponents(navController)
+                            Card(
+
+                                modifier = Modifier
+                                    .padding(10.dp)
+                                    .fillMaxWidth()
+                                    .wrapContentHeight(),
+//                                    clickable { cardViewCallBack(context,name+description) }
+
+                                shape = MaterialTheme.shapes.medium,
+                                elevation = 5.dp,
+                                backgroundColor = MaterialTheme.colors.surface
+                            ) {
+
+                                Row (
+                                    verticalAlignment = Alignment.CenterVertically
+
+                                ){
+
+                                    Image(painter = painterResource(R.drawable.ic_launcher_background),
+                                        contentDescription =null,
+                                        modifier= Modifier
+                                            .size(130.dp)
+                                            .padding(8.dp),
+                                        contentScale = ContentScale.Fit
+                                    )
+                                    Column(Modifier.padding(8.dp)) {
+
+                                        Text(text = "John Dow",
+                                            style= MaterialTheme.typography.h6,
+                                            color= MaterialTheme.colors.onSurface,
+                                            modifier= Modifier.padding(start=30.dp)
+                                        )
+                                        Text(text = "Plumber",
+                                            style= MaterialTheme.typography.body1,
+                                            color= MaterialTheme.colors.onSurface,
+                                            modifier= Modifier.padding(start=15.dp)
+                                        )
+
+                                    }
+
+                                }
+
+                            }
+                        }
+
 
                         Box(modifier = Modifier
                             .weight(0.8f)
