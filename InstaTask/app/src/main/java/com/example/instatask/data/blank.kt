6ba8359c1154f3379cc82bb451d0fe66164a1e21
linpeterk <100219056+linpeterk@users.noreply.github.com>
2022-03-28 14:27:11 -0700

    Revert "ew"
    This reverts commit caf477c4c46265cbad3e52a747a801204c4b5b71.

--- InstaTask/app/src/main/java/com/example/instatask/data/blank.kt
+++
@@ -1,4 +0,0 @@
-package com.example.instatask.data
-
-class blank {
-}
--- InstaTask/app/src/main/java/com/example/instatask/ui/theme/Color.kt
+++
@@ -1,9 +0,0 @@
-package com.example.instatask.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF2DC5C5)
-val graySurface = Color(0xFF202124)
