4921cd5f396367feb6418cab84e128b23f7ea882
Carlos Castellanos <96276293+carloscastellanos271@users.noreply.github.com>
2022-03-29 20:11:00 -0700

    TopApp bar

--- InstaTask/app/src/main/java/com/example/instatask/ui/app/MainActivity.kt
+++ InstaTask/app/src/main/java/com/example/instatask/ui/app/MainActivity.kt
@@ -25,8 +25,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
@@ -284,4 +287,18 @@ data class UserInfo(val name:String,val email:String,val address:String,val zip:
 data class Activity(val oldActivity:Array<String>,val currentActivity:Array<String>)
 
 
+//TopBar stuff
+@Composable
+fun TopBar() {
+    TopAppBar(
+        title = { Text(text = stringResource(R.string.app_name), fontSize = 18.sp) },
+        backgroundColor = colorResource(id = R.color.purple_700),
+        contentColor = Color.White
+    )
+}
 
+@Preview(showBackground = true)
+@Composable
+fun TopBarPreview() {
+    TopBar()
+}
