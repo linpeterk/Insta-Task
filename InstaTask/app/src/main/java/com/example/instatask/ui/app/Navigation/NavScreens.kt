efe955e244d7f2c269e6068c45752cf41c16fa8d
Carlos Castellanos <96276293+carloscastellanos271@users.noreply.github.com>
2022-03-30 20:50:54 -0700

    gigpic2

--- InstaTask/app/src/main/java/com/example/instatask/ui/app/Navigation/NavScreens.kt
+++ InstaTask/app/src/main/java/com/example/instatask/ui/app/Navigation/NavScreens.kt
@@ -3,7 +3,7 @@ package com.example.instatask.ui.app.Navigation
 import com.example.instatask.R
 
 sealed class NavScreens(var route: String, var icon: Int, var title: String) {
-    object TaskBoard : NavScreens("TaskBoard", R.drawable.farming, "Gigs")
+    object TaskBoard : NavScreens("TaskBoard", R.drawable.gigpic2, "Gigs")
     object WhenJob : NavScreens("WhenJob", R.drawable.toilet, "WhenJob")
     object SkillBoard : NavScreens("SkillBoard", R.drawable.deliveryman, "Professional")
     object Books : NavScreens("books", R.drawable.ic_sea_icon, "Books")
