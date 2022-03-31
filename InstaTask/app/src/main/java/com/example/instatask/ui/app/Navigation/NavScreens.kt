f10cb25aa73f03352c96db51dbbfac8797ca555b
Carlos Castellanos <96276293+carloscastellanos271@users.noreply.github.com>
2022-03-31 07:47:17 -0700

    Update NavScreens.kt

--- InstaTask/app/src/main/java/com/example/instatask/ui/app/Navigation/NavScreens.kt
+++ InstaTask/app/src/main/java/com/example/instatask/ui/app/Navigation/NavScreens.kt
@@ -6,7 +6,7 @@ sealed class NavScreens(var route: String, var icon: Int, var title: String) {
     object TaskBoard : NavScreens("TaskBoard", R.drawable.gigpic2, "Gigs")
     object WhenJob : NavScreens("WhenJob", R.drawable.toilet, "WhenJob")
     object SkillBoard : NavScreens("SkillBoard", R.drawable.deliveryman, "Professional")
-    object Books : NavScreens("books", R.drawable.ic_sea_icon, "Books")
+    object Books : NavScreens("books", R.drawable.ic_sea_icon, "Earnings")
     object Profile : NavScreens("profile", R.drawable.profile, "Profile")
 
 }
