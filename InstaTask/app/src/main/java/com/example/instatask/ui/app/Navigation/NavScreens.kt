c9f0dce842d4c17fd69ba5965a7416aa920d1eb1
Carlos Castellanos <96276293+carloscastellanos271@users.noreply.github.com>
2022-04-07 12:41:56 -0700

    button nav pics

--- InstaTask/app/src/main/java/com/example/instatask/ui/app/Navigation/NavScreens.kt
+++ InstaTask/app/src/main/java/com/example/instatask/ui/app/Navigation/NavScreens.kt
@@ -4,13 +4,13 @@ import com.example.instatask.R
 
 sealed class NavScreens(var route: String, var icon: Int, var title: String) {
     object TaskBoard : NavScreens("TaskBoard", R.drawable.gigpic2, "Gigs")
-    object SkillBoard : NavScreens("SkillBoard", R.drawable.deliveryman, "Professional")
+    object SkillBoard : NavScreens("SkillBoard", R.drawable.professionalimagethree, "Professional")
     object WhenJob : NavScreens("WhenJob", R.drawable.toilet, "WhenJob")
     object Books : NavScreens("books", R.drawable.ic_sea_icon, "Earnings")
-    object Profile : NavScreens("Profile", R.drawable.user, "Profile")
+    object Profile : NavScreens("Profile", R.drawable.profileimagetwo, "Profile")
     object WhenSkill: NavScreens("WhenSkill", R.drawable.ic_sea_icon, "WhenSkill")
-    object PostTask: NavScreens("PostTask", R.drawable.workinprogress, "Post Task")
-    object Gig: NavScreens("Gig", R.drawable.startup, "Gig")
+    object PostTask: NavScreens("PostTask", R.drawable.taskimagethree, "Post Task")
+    object Gig: NavScreens("Gig", R.drawable.gigimageone, "Gig")
 
     object Landing: NavScreens("Landing", R.drawable.ic_sea_icon, "Landing")
     object Login: NavScreens("Login", R.drawable.ic_sea_icon, "Login")
